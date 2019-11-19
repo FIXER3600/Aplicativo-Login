3ad9915bc4e64c8f2c6aa65099d3ca140faff04e
FIXER3600 <guilhermecomercial51@gmail.com>
2019-11-18 22:22:09 -0300

    first commit

---
+++ www/firebaseConfig.js
@@ -0,0 +1,9 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyAgPVqnui9ftnmBYPYS1c3tJ8P65NBgzY4",
+    authDomain: "aula10-7b642.firebaseapp.com",
+    databaseURL: "https://aula10-7b642.firebaseio.com",
+    projectId: "aula10-7b642",
+    storageBucket: "aula10-7b642.appspot.com",
+    messagingSenderId: "171720898343",
+    
+  };
